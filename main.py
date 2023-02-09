b614187c0eb3e7b27858c05fd51af5a4f9af1676
Kp Kumar <kumarkk.2220@gmail.com>
2023-02-09 23:50:53 +0530

    ToDo: Added the "Complete" feature to the ToDo list program
    Signed-off-by: Kp Kumar <kumarkk.2220@gmail.com>

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ print("Making a ToDo app using Python")
 todo_list = []
 
 while True:
-    user_input = input("Type add, show, edit or exit: ").strip().lower()
+    user_input = input("Type add, show, edit, complete or exit: ").strip().lower()
 
     match user_input:
         case 'add':
@@ -13,21 +13,28 @@ while True:
             # Adding enumerate
             # Adding Fstrings
             for index, item in enumerate(todo_list):
-                print(f"{index} -{item.title()}")
+                print(f"{index + 1}-{item.title()}")
         case 'exit':
             print("Now your ToDo items are :")
             for index, item in enumerate(todo_list):
-                print(f"{index} -{item.title()}")
+                print(f"{index + 1}-{item.title()}")
             break
         case 'edit':
             for index, item in enumerate(todo_list):
-                print(f"{index} -{item.title()}")
+                print(f"{index + 1}-{item.title()}")
             edit_item_number = int(input("Now select the item number you want to edit: ")) - 1
             new_edited_item = input("Enter a new item name: ")
             todo_list[edit_item_number] = new_edited_item
             print("The new edited item is ", new_edited_item)
             for item in todo_list:
                 print(item)
+        # Adding "Complete" feature for the To-Do
+        case 'complete':
+            print("Your list is as follows")
+            for index, item in enumerate(todo_list):
+                print(f"{index + 1}-{item.title()}")
+            complete_number = int(input("Enter the number of the list item to be completed: "))
+            todo_list.pop(complete_number - 1)
         case _:
             print("Unknown command received")
 
