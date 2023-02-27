eec57de49d323eb21c5fe3911722e510353a327e
Prasanna Kumar K <prasanna@insyncai.com>
2023-02-27 20:09:33 +0530

    Revert "ToDo List App: Used 'try' & 'except' and added few bonus and practice files"
    This reverts commit 3f4716ec

--- ToDo-App-Practice/practice_main.py
+++ ToDo-App-Practice/practice_main.py
@@ -1,51 +1,49 @@
 while True:
-    selections = ["add", "edit", "show", "complete", "exit"]
-    actions = ["To add a to-do item", "To edit a to-do item", "To show the todo list", "To mark an item complete",
-               "To stop"]
+    choices = ["Add", "Edit", "Show", "Complete", "Exit"]
+    actions = ["To Add a To-Do Item", "To Edit a To-Do Item", "To Show list", "To Complete a To-Do Item", "To Exit"]
 
-    for selection, action in zip(selections, actions):
-        print(f"{selection} - {action}")
+    for choice, action in zip(choices, actions):
+        print(f"{choice} {action}: ")
 
-    user_selection = input("Choose one of the actions from above list: ").strip().lower()
+    user_choice = input().lower().strip()
 
-    if 'add' in user_selection:
-        with open("todos.txt", 'r') as file:
+    if 'add' in user_choice or 'new' in user_choice:
+        todo_item = user_choice[4:] + "\n"
+
+        with open("todos.txt", "r") as file:
             todos = file.readlines()
 
-        todos.append(user_selection[4:] + "\n")
+        todos.append(todo_item)
 
         with open("todos.txt", 'w') as file:
             file.writelines(todos)
 
-    elif 'edit' in user_selection:
-        user_edit = int(user_selection[5:])
-
+    elif 'show' in user_choice:
+        with open("todos.txt", 'r') as file:
+            todos = file.readlines()
+            todos = [item.strip("\n") for item in todos]
+            for index, todo in enumerate(todos):
+                print(f"{index + 1}-{todo}")
+    elif 'edit' in user_choice:
         with open("todos.txt", 'r') as file:
             todos = file.readlines()
 
-        new_item = input("Enter the new item name")
-        todos[user_edit - 1] = new_item + "\n"
+        edit_item_name = input("Enter the new edit name")
+        todos[int(user_choice[5:]) - 1] = edit_item_name + "\n"
+
+        with open('todos.txt', 'w') as file:
+            file.writelines(todos)
+
+    elif 'complete' in user_choice:
+        with open("todos.txt", 'r') as file:
+            todos = file.readlines()
+
+        todos.pop(int(user_choice[9:]) - 1)
 
         with open("todos.txt", 'w') as file:
             file.writelines(todos)
-    elif 'show' in user_selection:
-        with open("todos.txt", 'r') as file:
-            todos = file.readlines()
 
-        todos = [item.strip("\n") for item in todos]
-
-        for index, todo in enumerate(todos):
-            print(f"{index+1}-{todo}")
-    elif 'complete' in user_selection:
-        with open("todos.txt", 'r') as file:
-            todos = file.readlines()
-
-        completed_item = int(user_selection[8:])
-        todos.pop(completed_item - 1)
-        with open("todos.txt", 'w') as file:
-            file.writelines(todos)
-    elif 'exit' in user_selection:
-        print("Users exits from the prompts")
+    elif 'exit' in user_choice:
         break
     else:
-        print("Invalid option selected")
+        print("Invalid command received")
