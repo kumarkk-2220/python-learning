79169f8922d1d3fece7d29445b9c86db3a3df7d7
kumarkk-2220 <kumarkk.2220@gmail.com>
2023-02-27 20:47:41 +0530

    ToDo List App: Used Functions, Parameters and Arguments

--- 0Main Project/main.py
+++ 0Main Project/main.py
@@ -4,53 +4,52 @@ def get_todos(filepath):
     return todos_local
 
 
+def write_todos(filepath, todos_arg):
+    with open(filepath, 'w') as file:
+        file.writelines(todos_arg)
+
+
 while True:
     user_selection = input("Type add, edit, show, complete or exit: ").strip().lower()
 
     if user_selection.startswith("add"):
-        todos = get_todos("todos.txt")
+        todos = get_todos(filepath="todos.txt")
 
         todos.append(user_selection[4:] + "\n")
 
-        with open("todos.txt", 'w') as file:
-            file.writelines(todos)
+        write_todos(filepath="todos.txt", todos_arg=todos)
 
     elif user_selection.startswith("edit"):
         try:
             user_edit = int(user_selection[5:])
 
-            todos = get_todos("todos.txt")
+            todos = get_todos(filepath="todos.txt")
 
             new_item = input("Enter the new item name: ")
             todos[user_edit - 1] = new_item + "\n"
 
-            with open("todos.txt", 'w') as file:
-                file.writelines(todos)
+            write_todos(filepath="todos.txt", todos_arg=todos)
         except ValueError:
             print("Your command is not valid")
     elif user_selection.startswith("show"):
-        todos = get_todos("todos.txt")
+        todos = get_todos(filepath="todos.txt")
 
         todos = [item.strip("\n") for item in todos]
 
         for index, todo in enumerate(todos):
-            print(f"{index+1}-{todo}")
+            print(f"{index + 1}-{todo}")
     elif user_selection.startswith("complete"):
         try:
-            todos = get_todos("todos.txt")
+            todos = get_todos(filepath="todos.txt")
 
             completed_item = int(user_selection[8:])
             todos.pop(completed_item - 1)
-            with open("todos.txt", 'w') as file:
-                file.writelines(todos)
+            write_todos(filepath="todos.txt", todos_arg=todos)
         except IndexError:
             print("No items with that number")
             continue
     elif user_selection.startswith("exit"):
-        print("Users exits from the prompts")
+        print("Exiting from the prompts")
         break
     else:
         print("Invalid option selected")
-
-
-        ### Adding a test line
