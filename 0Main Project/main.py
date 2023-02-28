5e009700b5fe7298a971cd0727a77b0e107e967d
kumarkk-2220 <kumarkk.2220@gmail.com>
2023-02-28 16:36:53 +0530

    ToDo List App: Added if-name-main to th program

--- 0Main Project/main.py
+++ 0Main Project/main.py
@@ -1,40 +1,30 @@
-def get_todos(filepath="todos.txt"):
-    """ Read a text file and return the list of to-do items """
-    with open(filepath, 'r') as file_local:
-        todos_local = file_local.readlines()
-    return todos_local
-
-
-def write_todos(todos_arg, filepath="todos.txt"):
-    """ Write the to-do items in the list in the text file. """
-    with open(filepath, 'w') as file:
-        file.writelines(todos_arg)
-
+# from functions import get_todos, write_todos
+import functions
 
 while True:
     user_selection = input("Type add, edit, show, complete or exit: ").strip().lower()
 
     if user_selection.startswith("add"):
-        todos = get_todos()
+        todos = functions.get_todos()
 
         todos.append(user_selection[4:] + "\n")
 
-        write_todos(todos_arg=todos)
+        functions.write_todos(todos_arg=todos)
 
     elif user_selection.startswith("edit"):
         try:
             user_edit = int(user_selection[5:])
 
-            todos = get_todos()
+            todos = functions.get_todos()
 
             new_item = input("Enter the new item name: ")
             todos[user_edit - 1] = new_item + "\n"
 
-            write_todos(todos_arg=todos)
+            functions.write_todos(todos_arg=todos)
         except ValueError:
             print("Your command is not valid")
     elif user_selection.startswith("show"):
-        todos = get_todos()
+        todos = functions.get_todos()
 
         todos = [item.strip("\n") for item in todos]
 
@@ -42,11 +32,11 @@ while True:
             print(f"{index + 1}-{todo}")
     elif user_selection.startswith("complete"):
         try:
-            todos = get_todos()
+            todos = functions.get_todos()
 
             completed_item = int(user_selection[8:])
             todos.pop(completed_item - 1)
-            write_todos(todos_arg=todos)
+            functions.write_todos(todos_arg=todos)
         except IndexError:
             print("No items with that number")
             continue
