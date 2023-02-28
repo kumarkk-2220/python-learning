000b52d9f7505d5f8c782b92629902cd9763bb2b
kumarkk-2220 <kumarkk.2220@gmail.com>
2023-02-28 15:29:21 +0530

    ToDo List App: Practice files on functions and default parameters/arguments

--- BonusExamples/8Decoupling.py
+++ BonusExamples/8Decoupling.py
@@ -1,17 +1,22 @@
 feet_inches = input("Enter feet and inches: ")
 
 
-def feet_to_meters(feet_inches):
+def parse(feet_inches):
     parts = feet_inches.split(" ")
     feet = float(parts[0])
     inch = float(parts[1])
+    return {"feet": feet, "inch": inch}
 
+
+def feet_to_meters(feet, inch):
     meters = feet * 0.3048 + inch * 0.0254
     return meters
 
 
 try:
-    result = feet_to_meters(feet_inches)
+    result = feet_to_meters(parse(feet_inches)["feet"], parse(feet_inches)["inch"])
+
+    print(f"{parse(feet_inches)['feet']} feet and {parse(feet_inches)['inch']} inches is equal to {result}")
 
     if result < 1:
         print("Kid is too small!")
