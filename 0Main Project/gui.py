120feabe9f86f18b3c4ab4d63fad6aed9582bbda
Prasanna Kumar (kumarkk-2220) <kumarkk.2220@gmail.com>
2023-03-17 20:16:39 +0530

    Added "complete" and "exit" feature to the app

--- 0Main Project/gui.py
+++ 0Main Project/gui.py
@@ -9,9 +9,15 @@ list_box = sg.Listbox(values=functions.get_todos(),
                       enable_events=True,
                       size=[45, 10])
 edit_button = sg.Button("Edit")
+complete_button = sg.Button("Complete", key='complete')
+exit_button = sg.Button("Exit", key="Exit")
 
 window = sg.Window("My To-Do App",
-                   layout=[[label], [input_box, add_button], [list_box, edit_button]],
+                   layout=[[label],
+                           [input_box, add_button],
+                           [list_box, edit_button, complete_button],
+                           [exit_button]
+                           ],
                    font=('Helvetica', 15))
 
 while True:
@@ -34,6 +40,16 @@ while True:
             todos[index] = new_todo
             functions.write_todos(todos)
             window['todos'].update(values=todos)
+        case "Complete":
+            todo_to_complete = values['todos'][0]
+            todos = functions.get_todos()
+            index = todos.index(todo_to_complete)
+            todos.remove(index)
+            functions.write_todos(todos_arg=todos)
+            window['todos'].update(values=todos)
+            window['todo'].update(value='')
+        case "Exit":
+            break
         case 'todos':
             window['todo'].update(value=values['todos'][0])
         case sg.WIN_CLOSED:
